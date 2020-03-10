22b384cd854ffae8641259fa74bd3461c8d0e4fa
BaiCunCi <18320290214@163.com>
2020-03-10 18:39:06 +0800

    write your meaaage

---
+++ src/main/java/com/zq/community/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.zq.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @ProjectName: community
+ * @ClassName: HelloController
+ * @Author: 余
+ * @Date: 2020/3/10 18:02
+ * @Description: 一句话描述该类的功能
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
